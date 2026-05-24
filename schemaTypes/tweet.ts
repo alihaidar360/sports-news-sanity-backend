d097be8423374483212a4fa7fbe2a71a8899d22b
Ali Haidar <ecommerceedittor@gmail.com>
2026-05-24 08:24:37 +0500

    tweet update

--- schemaTypes/tweet.ts
+++ schemaTypes/tweet.ts
@@ -7,66 +7,10 @@ export const tweet = defineType({
 
   fields: [
     defineField({
-      name: "author",
-      title: "Author",
-      type: "string",
-    }),
-
-    defineField({
-      name: "handle",
-      title: "Handle",
-      type: "string",
-    }),
-
-    defineField({
-      name: "content",
-      title: "Content",
-      type: "text",
-    }),
-
-    defineField({
-      name: "avatar",
-      title: "Avatar",
-      type: "image",
-      options: {
-        hotspot: true,
-      },
-    }),
-
-    defineField({
-      name: "verified",
-      title: "Verified",
-      type: "boolean",
-    }),
-
-    defineField({
-      name: "likes",
-      title: "Likes",
-      type: "number",
-    }),
-
-    defineField({
-      name: "reposts",
-      title: "Reposts",
-      type: "number",
-    }),
-
-    defineField({
-      name: "replies",
-      title: "Replies",
-      type: "number",
-    }),
-
-    defineField({
-      name: "time",
-      title: "Time",
-      type: "string",
-    }),
-
-    defineField({
       name: "url",
       title: "Tweet URL",
       type: "url",
+      validation: (Rule) => Rule.required(),
     }),
 
     defineField({
