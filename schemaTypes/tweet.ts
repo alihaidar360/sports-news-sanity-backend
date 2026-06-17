4327a5eb9e5954f3aff48cf5419fc5aa38acd5a9
Ali Haidar <ecommerceedittor@gmail.com>
2026-06-17 22:25:05 +0500

    Tweet section updated

--- schemaTypes/tweet.ts
+++ schemaTypes/tweet.ts
@@ -1,37 +1,3 @@
-// import { defineType, defineField } from "sanity";
-
-// export const tweet = defineType({
-//   name: "tweet",
-//   title: "Tweets",
-//   type: "document",
-
-//   fields: [
-//     defineField({
-//       name: "url",
-//       title: "Tweet URL",
-//       type: "url",
-//       validation: (Rule) => Rule.required(),
-//     }),
-
-//     defineField({
-//       name: "sport",
-//       title: "Sport",
-//       type: "reference",
-//       to: [{ type: "sportCategory" }],
-//     }),
-//   ],
-
-//   preview: {
-//     select: {
-//       title: "url",
-//       subtitle: "sport.name",
-//     },
-//   },
-// });
-
-
-
-
 import { defineType, defineField } from "sanity";
 
 export const tweet = defineType({
