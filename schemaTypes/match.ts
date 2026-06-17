3da3636726255397ccb0a2ad3c433cba17ea4ce1
Ali Haidar <ecommerceedittor@gmail.com>
2026-06-18 02:12:52 +0500

    match update

--- schemaTypes/match.ts
+++ schemaTypes/match.ts
@@ -1,46 +1,53 @@
-import { defineType, defineField } from "sanity";
+import { defineType, defineField } from 'sanity'
 
 export const match = defineType({
-  name: "match",
-  title: "Matches",
-  type: "document",
-
+  name: 'match',
+  title: 'Matches',
+  type: 'document',
   fields: [
     defineField({
-      name: "team1",
-      title: "Team 1",
-      type: "string",
+      name: 'team1',
+      title: 'Team 1',
+      type: 'string',
     }),
-
     defineField({
-      name: "team2",
-      title: "Team 2",
-      type: "string",
+      name: 'team2',
+      title: 'Team 2',
+      type: 'string',
     }),
-
     defineField({
-      name: "date",
-      title: "Match Date",
-      type: "datetime",
+      name: 'team1Logo',
+      title: 'Team 1 Logo',
+      type: 'image',
+      options: { hotspot: true },
     }),
-
     defineField({
-      name: "tournament",
-      title: "Tournament",
-      type: "string",
+      name: 'team2Logo',
+      title: 'Team 2 Logo',
+      type: 'image',
+      options: { hotspot: true },
     }),
-
+    
     defineField({
-      name: "stadium",
-      title: "Stadium",
-      type: "string",
+      name: 'date',
+      title: 'Match Date',
+      type: 'datetime',
     }),
-
     defineField({
-      name: "sport",
-      title: "Sport",
-      type: "reference",
-      to: [{ type: "sportCategory" }],
+      name: 'tournament',
+      title: 'Tournament',
+      type: 'string',
+    }),
+    defineField({
+      name: 'stadium',
+      title: 'Stadium',
+      type: 'string',
+    }),
+    defineField({
+      name: 'sport',
+      title: 'Sport',
+      type: 'reference',
+      to: [{ type: 'sportCategory' }],
     }),
   ],
-});
+})
